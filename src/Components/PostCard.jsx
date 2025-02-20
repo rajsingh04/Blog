7c3e311d3dcae3ab8842a7166d214b98823d9897
raj singh <rajsingh18904@gmail.com>
2025-02-21 05:11:20 +0530

    origin chagned

--- src/Components/PostCard.jsx
+++ src/Components/PostCard.jsx
@@ -3,7 +3,6 @@ import appwriteService from "../appwrite/config";
 import { Link } from "react-router-dom";
 
 const PostCard = ({ $id, title, featuredImage }) => {
-	console.log($id);
 	return (
 		<Link to={`/post/${$id}`}>
 			<div className="w-full bg-gray-100 rounded-xl p-4">
